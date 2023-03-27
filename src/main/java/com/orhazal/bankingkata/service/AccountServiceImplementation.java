9d178bee2a52d03de890073ff65e97c865a7ea5a
Omar <omar.rhazal@gmail.com>
2023-03-27 16:15:54 +0200

    Start of US1

--- src/main/java/com/orhazal/bankingkata/service/AccountServiceImplementation.java
+++ src/main/java/com/orhazal/bankingkata/service/AccountServiceImplementation.java
@@ -7,9 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.orhazal.bankingkata.domain.Operation;
 import com.orhazal.bankingkata.domain.enums.OperationType;
+import com.orhazal.bankingkata.repository.AccountRepository;
+import com.orhazal.bankingkata.repository.OperationRepository;
+
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class AccountServiceImplementation implements AccountService {
+
+	private AccountRepository accountRepository;
+	private OperationRepository operationRepository;
 
 	@Override
 	public Operation processOperation(OperationType type, BigDecimal amount, Long accountId) {
