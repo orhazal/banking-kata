a43f5f928011b60875f9597f5fab98b189b261e5
Omar <omar.rhazal@gmail.com>
2023-03-27 12:55:52 +0200

    Initial commit after Spring Boot Init

---
+++ src/main/java/com/orhazal/bankingkata/BankingkataApplication.java
@@ -0,0 +1,13 @@
+package com.orhazal.bankingkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingkataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingkataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orhazal/bankingkata/BankingkataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orhazal.bankingkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingkataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
